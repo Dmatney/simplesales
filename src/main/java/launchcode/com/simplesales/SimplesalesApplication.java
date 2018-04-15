7fe63d0cec68d356fd4caeb5569d6669a745d156
Dave Matney <davematney@dem3.com>
2018-04-14 21:11:50 -0500

    Confirmed basic function of springboot

--- src/main/java/launchcode/com/simplesales/SimplesalesApplication.java
+++ src/main/java/launchcode/com/simplesales/SimplesalesApplication.java
@@ -8,5 +8,6 @@ public class SimplesalesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimplesalesApplication.class, args);
+		System.out.println("Simple Sales Application Up");
 	}
 }
