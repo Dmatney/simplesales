5a4ce4c91f5ea1ebc6aacb98aef0f3980ad3c7e8
Dave Matney <davematney@dem3.com>
2018-04-14 20:41:46 -0500

    adding a base which works

---
+++ src/main/java/launchcode/com/simplesales/SimplesalesApplication.java
@@ -0,0 +1,12 @@
+package launchcode.com.simplesales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplesalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplesalesApplication.class, args);
+	}
+}
---
+++ src/test/java/launchcode/com/simplesales/SimplesalesApplicationTests.java
@@ -0,0 +1,16 @@
+package launchcode.com.simplesales;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimplesalesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
